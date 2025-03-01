d3cf2535e3998e33fed43a902b3e4bb73d82e452
adstre <camila.rodriguez.2023@cbtis-246.edu.mx>
2025-02-28 18:12:27 -0600

    Update hockey.sql

--- hockey.sql
+++ hockey.sql
@@ -93,3 +93,34 @@ CREATE TABLE roundstats (
     sogLeaderId int (2) NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+INSERT into playoffround (id,team1Id,team2Id,startTime, endTime, roundNum)
+values (1,1,2,'2025-02-25 19:36','2025-02-25 20:21',3);
+
+INSERT into hockeygame (id, roundld, startTime, description, team1Id, team2Id)
+values (1, 1, '2025-02-27 20:35', 'Quarterfinals', 1, 2);
+
+INSERT into gamescores (id, team1Id, team2Id)
+values (1, 2, 2);
+
+INSERT into roundstats (id, numShutouts, goalLeaderId, assistLeaderId , penaltyLeaderId, plusMinusId, faceoffsWonLeaderId,  sogLeaderId)
+values (1, 3, 4, 4, 8, 3, 14, 20);
+
+INSERT into userstatspicks (id, round1, goalLeaderId, assistLeaderId, penaltyLeaderId, plusMinusId, faceoffsWonLeaderId, sogLeaderId, numShutouts, userId)
+values (1, 1, 10, 15, 12, 8, 14, 20, 2, 2);
+
+INSERT into user (id, username, password)
+values (3, 'CANDYX', 'MessiT');
+
+INSERT INTO userscorepicks (id, hockeyGameId, team1IdScore, team2IdScore, userId)
+values (1, 1, 2, 2, 1);
+
+INSERT into hockeyteam (id, name, logo)
+values(1, 'Team A', 'oxa');
+
+INSERT into hockeyteamplayer (id, hockeyTeamId, firstName, lastName, jerseyNum)
+values (1, 1, 'Aseret', 'Camila', 8);
+
+INSERT into  userinfo (id, firstName, lastName, email, round1Points, round2Points, round3Points, round4Points)
+values (1, 'Aseret', 'Camila', 'camila.xin611@gmail.com', 0, 1, 1, 2);
